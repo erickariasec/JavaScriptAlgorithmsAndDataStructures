70ba88ac86d4de67f145ee41945b606209bfa31c
erickariasec <erickarias1999@hotmail.com>
2022-05-29 22:31:03 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, C001 - Comment Your Code in JavaScript

---
+++ BasicJavaScript/C001_CommentYourCodeInJavaScript/comments.js
@@ -0,0 +1,6 @@
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+// ! As you code, you should regularly add comments to clarify how parts of your code work. A good comment can help communicate the intent of your code, both to others and to your future self.
