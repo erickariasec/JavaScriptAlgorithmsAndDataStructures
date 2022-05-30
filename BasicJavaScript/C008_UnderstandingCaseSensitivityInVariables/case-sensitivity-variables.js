1471f700f4cd12436d12ec5e3a5009bf4d74120c
erickariasec <erickarias1999@hotmail.com>
2022-05-30 12:00:55 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, C008 - Understanding Case Sensitivity in Variables

---
+++ BasicJavaScript/C008_UnderstandingCaseSensitivityInVariables/case-sensitivity-variables.js
@@ -0,0 +1,9 @@
+// Declaring variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
