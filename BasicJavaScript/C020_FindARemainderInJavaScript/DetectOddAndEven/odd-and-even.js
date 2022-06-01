a10fe524c3ec98e066f3f80d5f66bcac11ab5905
erickariasec <erickarias1999@hotmail.com>
2022-06-01 12:02:07 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, C020 - Find a Remainder in JavaScript, Detect Odd and Even

---
+++ BasicJavaScript/C020_FindARemainderInJavaScript/DetectOddAndEven/odd-and-even.js
@@ -0,0 +1,14 @@
+const num1 = 5;
+const num2 = 10;
+
+if (num1 % 2 === 0) {
+    console.log(num1 + " is even");
+} else {
+    console.log(num1 +" is odd");
+}
+
+if (num2 % 2 === 0) {
+    console.log(num2 + " is even");
+} else {
+    console.log(num2 +" is odd");
+}
