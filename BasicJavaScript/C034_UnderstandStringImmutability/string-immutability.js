4a8dfa5e003c64fed23f6cb97472e9542c1f599f
erickariasec <erickarias1999@hotmail.com>
2022-06-01 22:31:36 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, C034 - Understand String Immutability

---
+++ BasicJavaScript/C034_UnderstandStringImmutability/string-immutability.js
@@ -0,0 +1,20 @@
+/*
+    In JavaScript, String values are immutable, which means that they cannot be altered once created.
+
+    For example, the following code:
+
+        let myStr = "Bob";
+        myStr[0] = "J";
+
+    cannot change the value of myStr to Job, because the contents of myStr cannot be altered. Note that this does not mean that myStr cannot be changed, just that the individual characters of a string literal cannot be changed. The only way to change myStr would be to assign it with a new string, like this:
+
+        let myStr = "Bob";
+        myStr = "Job";
+*/
+
+// Configuration
+let myStr = "Jello World";
+
+// Just change the code bellow
+myStr = "Hello World"; // Change this line
+// Just change the code above
