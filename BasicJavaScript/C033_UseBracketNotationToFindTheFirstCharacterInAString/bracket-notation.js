ca0465f448b232e717c1cddcfd7dab24f3e31f1c
erickariasec <erickarias1999@hotmail.com>
2022-06-01 22:25:31 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, C033 - Use Bracket Notation to Find the First Character in a String

---
+++ BasicJavaScript/C033_UseBracketNotationToFindTheFirstCharacterInAString/bracket-notation.js
@@ -0,0 +1,8 @@
+// Configuration
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Change only the code below
+firstLetterOfLastName = lastName[0]; // Change this line
+
+console.log(firstLetterOfLastName);
