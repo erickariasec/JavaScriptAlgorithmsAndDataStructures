325fd6a9f86cf47838a2eabcd3996072880c80f6
erickariasec <erickarias1999@hotmail.com>
2022-06-08 18:58:39 -0500

    freeCodeCamp, JavaScript Algorithms and Data Structures, Basic JavaScript, C064 - Comparison with the Strict Inequality Operator

---
+++ BasicJavaScript/C064_ComparisonWithTheStrictInequalityOperator/comparison-strict-inequality-operator.js
@@ -0,0 +1,24 @@
+/*
+    The strict inequality operator (!==) is the logical opposite of the strict equality operator. It means "Strictly Not Equal" and returns false where strict equality would return true and vice versa. The strict inequality operator will not convert data types.
+
+    Examples
+
+        3 !==  3  // false
+        3 !== '3' // true
+        4 !==  3  // true
+
+    ? Add the strict inequality operator to the if statement so the function will return the string Not Equal when val is not strictly equal to 17
+*/
+
+// Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+        // Change this line
+        return "Not Equal to 17";
+    }
+    return "Equal to 17";
+}
+
+console.log(testStrictNotEqual(10));
+console.log(testStrictNotEqual(17));
+console.log(testStrictNotEqual("17"));
